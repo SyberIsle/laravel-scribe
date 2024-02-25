674b921dd1c9ecfe0bd82255be9168c645a1566e
David Lundgren <dlundgren@syberisle.net>
2024-02-25 14:07:33 -0600

    Updates the way the logging is written.
    Style updates for the classes and migration

--- src/Model/Log.php
+++ src/Model/Log.php
@@ -11,7 +11,21 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Support\Facades\Auth;
+use Ramsey\Uuid\Uuid;
 
+/**
+ * @property int|Uuid           $id
+ * @property Model              $causer
+ * @property int                $causerId
+ * @property string             $causerType
+ * @property mixed              $context
+ * @property \DateTimeImmutable $createdOn
+ * @property int                $level
+ * @property string             $message
+ * @property Model              $subject
+ * @property int                $subjectId
+ */
 abstract class Log
 	extends Model
 {
@@ -60,6 +74,18 @@ abstract class Log
 	}
 
 	/**
+	 * Override the parent to block updates.
+	 *
+	 * @param Builder $query
+	 *
+	 * @return mixed
+	 */
+	final protected function performUpdate(Builder $query)
+	{
+		throw new \RuntimeException("Updating logs is not allowed");
+	}
+
+	/**
 	 * Overrides the parent to prevent setting the updatedAt Column
 	 *
 	 * @param mixed $value The value to set
@@ -93,14 +119,37 @@ abstract class Log
 	}
 
 	/**
-	 * Override the parent to block updates.
+	 * Writes the log for the given subject
 	 *
-	 * @param Builder $query
+	 * @param Model                 $subject The subject to make the log for
+	 * @param string                $message The log message
+	 * @param int                   $level   The log level
+	 * @param ?array<string, mixed> $context Associated Context
 	 *
-	 * @return mixed
+	 * @return $this
 	 */
-	final protected function performUpdate(Builder $query)
+	final public function write(Model $subject, string $message, int $level = LOG_INFO, ?array $context = []): static
 	{
-		throw new \RuntimeException("Updating logs is not allowed");
+		if (!($subject instanceof $this->subjectModel)) {
+			throw new \BadMethodCallException("Subject must be {$this->subjectModel}");
+		}
+
+		$this->level   = $level;
+		$this->message = $message;
+		if ($causer = $context['causer'] ?? Auth::user()) {
+			$this->causer()->associate($causer);
+		}
+		unset($context['causer']);
+
+		if (!empty($context)) {
+			$this->context = $context;
+		}
+
+		$this->subject()->associate($subject);
+		if ($this->save()) {
+			return $this;
+		}
+
+		throw new \RuntimeException("Unable to save the log");
 	}
 }
